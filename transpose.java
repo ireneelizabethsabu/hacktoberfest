bce0712dfec76621b5f62c6cae46fc55279ce38d
ireneelizabethsabu <56963461+ireneelizabethsabu@users.noreply.github.com>
2020-10-01 14:58:41 +0530

    Update transpose.java

--- transpose.java
+++ transpose.java
@@ -1,32 +1,28 @@
 import java.util.Scanner;
-public class transpose {
-    public static void main(String[] args) {
-        int i, j, temp;
-        Scanner s = new Scanner(System.in);
-        System.out.print("Enter the number of rows and columns in the matrix:");
-        int m = s.nextInt();
-        int n = s.nextInt();
-        int a[][] = new int[m][n];
-        System.out.print("Enter the elements of the matrix:");
-        for (i = 0; i < m; i++) {
-            for (j = 0; j < n; j++)
-                a[i][j] = s.nextInt();
-        }
-        int t[][]=new int[n][m];
-        for (i = 0; i < m; i++)
-        {
-            for (j = 0; j < n; j++)
-            t[j][i]=a[i][j];
-        }
-        System.out.println("Transpose of the matrix");
-        for (i = 0; i < n; i++)
-        {
-            for (j = 0; j < m; j++)
-            {
-                System.out.print( t[i][j]+" ");
-            }
-            System.out.println();
-        }
-    }
-    
+
+class Transpose{
+
+	public static void main(String args[]){
+		
+		Scanner scanner= new Scanner(System.in);
+	
+		System.out.print("Enter the size of the matrix A: ");
+		int m = scanner.nextInt();
+		int n = scanner.nextInt();
+				
+		int[][] A = new int[m][n];
+		
+		System.out.println("Enter elements of matrix A: ");
+		for(int i=0;i<m;i++){
+			for(int j=0;j<n;j++)
+				A[i][j] = scanner.nextInt();
+		}
+		System.out.println("TRANSPOSE A: ");
+		for(int i=0;i<n;i++){
+			for(int j=0;j<m;j++){
+				System.out.print(A[j][i] + " ");
+			}
+			System.out.println(" ");
+		}
+	}
 }
