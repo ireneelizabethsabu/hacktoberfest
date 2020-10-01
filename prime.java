bad840f95de8e975d85578e1efef39fc1be51a47
ireneelizabethsabu <56963461+ireneelizabethsabu@users.noreply.github.com>
2020-10-01 14:54:50 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -1,22 +1,26 @@
-import java.io.*; 
 import java.util.Scanner;
-public class prme
-{
-	public static void main(String[] args) {
-    int i, n,count=0;
-    Scanner s = new Scanner(System.in);
-    n = s.nextInt();
-    for(i=1;i<=n;i++)
-{
-   if(n%i==0)
-      count++;
-}
-if(count==2)
-System.out.println("print number is prime");
-else
-System.out.println("the number is not prime"); 
-     
-  }
-	
-}
 
+class Prime{
+
+	public static void main(String args[]){
+		boolean flag=false;
+		Scanner scanner= new Scanner(System.in);
+	
+		System.out.print("Enter the number: ");
+		int n = scanner.nextInt();
+		if(n == 1)
+			System.out.println("1 is neither prime nor composite!!!");
+		else{
+			for(int i=2;i<=Math.sqrt(n);i++){
+				if(n%i==0){
+					System.out.println(n+" is not PRIME!!");
+					flag = true;
+					break;
+				}
+			}
+		}
+		if(!flag)
+			System.out.println(n + " is PRIME!!");
+
+	}
+}
